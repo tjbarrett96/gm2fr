fixed completely incorrect Histogram1D.moment uncertainty formula

Histogram1D.py
@@ -314,14 +314,18 @@ class Histogram1D:
     else:
       moment_factor = (self.centers - self.mean())**degree
 
+    moment = calc.area(self.centers, moment_factor * norm_heights)
+    if not error:
+      return moment
+
     # get covariance of the integrand: scale covariance of heights with moment factor
     if self.cov.ndim == 1:
-      scaled_cov = norm_cov * moment_factor**2
+      scaled_cov = norm_cov * (moment_factor - moment)**2
     else:
-      scaled_cov = norm_cov * np.outer(moment_factor, moment_factor)
+      scaled_cov = norm_cov * np.outer(moment_factor - moment, moment_factor - moment)
 
     # integrate (x - <x>)^n * rho(x)
-    return calc.area(self.centers, moment_factor * norm_heights, cov = scaled_cov if error else None)
+    return calc.area(self.centers, moment_factor * norm_heights, cov = scaled_cov)
 
   # ================================================================================================
 