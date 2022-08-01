4d949d36d2552c5728ec258135e538480ec05283
Tyler <tjb269@cornell.edu>
2022-08-01 12:17:02 -0400

    added range output and other systematics to process_systematic

--- scripts/process_systematic.py
+++ scripts/process_systematic.py
@@ -11,14 +11,20 @@ from gm2fr.scripts.plot_trend import plot_trend
 
 # ==================================================================================================
 
+# default labels for folders where systematic scan results are expected
 systematics_folder = {
   "start": "StartTimeScan",
   "end": "EndTimeScan",
   "df": "FrequencySpacingScan",
   "freq_width": "FrequencyWidthScan",
-  "inner_width": "InnerWidthScan"
+  "inner_width": "InnerWidthScan",
+  "fr_method": "FRMethodScan",
+  "bg_model": "BGModelScan",
+  "fr_rebin": "FRRebinScan"
 }
 
+# allowed ranges for parameters when computing std. dev. of systematic scan results
+# we may want to plot outside this range to show what happens at boundaries, but not use for computations
 limit_range = {
   "start": (4, 30),
   "end": (200, 400),
@@ -30,9 +36,18 @@ limit_range = {
 
 def process_systematic(dataset, systematic, variable, output = None, folder = None, skip = 1):
 
+  # get the default folder name for the systematic scan results, if none supplied
   folder = systematics_folder[systematic] if folder is None else folder
 
+  # read the systematic scan results for data from the results folder
   data_results = np.load(f"{io.results_path}/{dataset}/{folder}/results.npy", allow_pickle = True)
+  data_x, data_y, data_err_y = data_results[systematic], data_results[variable], data_results[f"err_{variable}"]
+
+  # remove misbehaved data points based on wildly unlikely C_E
+  data_mask = (data_results["c_e"] > 0) & (data_results["c_e"] < 1000)
+  data_x, data_y, data_err_y = data_x[data_mask], data_y[data_mask], data_err_y[data_mask]
+
+  # plot the data trend
   data_errorbar = plot_trend(
     systematic,
     variable,
@@ -41,9 +56,15 @@ def process_systematic(dataset, systematic, variable, output = None, folder = No
     skip = skip
   )
 
-  data_x, data_y, data_err_y = data_results[systematic], data_results[variable], data_results[f"err_{variable}"]
-
+  # read the systematic scan results for toy MC from the results folder
   sim_results = np.load(f"{io.results_path}/{dataset}/{folder}/sim_results.npy", allow_pickle = True)
+  sim_x, sim_y, sim_err_y = sim_results[systematic], sim_results[variable], sim_results[f"err_{variable}"]
+
+  # remove misbehaved data points based on wildly unlikely C_E
+  sim_mask = (sim_results["c_e"] > 0) & (sim_results["c_e"] < 1000)
+  sim_x, sim_y, sim_err_y = sim_x[sim_mask], sim_y[sim_mask], sim_err_y[sim_mask]
+
+  # plot the MC trend
   sim_errorbar = plot_trend(
     systematic,
     variable,
@@ -52,39 +73,49 @@ def process_systematic(dataset, systematic, variable, output = None, folder = No
     skip = skip
   )
 
-  sim_x, sim_y, sim_err_y = sim_results[systematic], sim_results[variable], sim_results[f"err_{variable}"]
-
+  # mask the trends within the appropriate range for this parameter, before computing std. dev.
   if systematic in limit_range:
-    mask = (data_x >= limit_range[systematic][0]) & (data_x <= limit_range[systematic][1])
-    data_x, data_y, data_err_y = data_x[mask], data_y[mask], data_err_y[mask]
-    sim_x, sim_y, sim_err_y = sim_x[mask], sim_y[mask], sim_err_y[mask]
+    range_mask = (data_x >= limit_range[systematic][0]) & (data_x <= limit_range[systematic][1])
+    data_x, data_y, data_err_y = data_x[range_mask], data_y[range_mask], data_err_y[range_mask]
+    sim_x, sim_y, sim_err_y = sim_x[range_mask], sim_y[range_mask], sim_err_y[range_mask]
 
+  # get the default output name, if path or PDF not specified
   if output is None:
     output = f"{io.plot_path}/{dataset}_{systematic}_plots.pdf"
 
+  # get the axis labels and units for the plot
   x_label = const.info[systematic].format_label() if systematic in const.info.keys() else systematic
   y_label = const.info[variable].format_symbol() if variable in const.info.keys() else variable
   y_unit = const.info[variable].units if variable in const.info.keys() else ""
   if y_unit is None:
     y_unit = ""
 
+  # apply labels, legend, and save to output
   style.label_and_save(x_label, y_label, output)
 
+  # iterate over two modes to plot: difference between data and simulation ("diff"), and raw data trend alone ("normal")
   for syst_y, syst_err_y, mode in ((data_y - sim_y, np.sqrt(data_err_y**2 + sim_err_y**2), "diff"), (data_y, data_err_y, "normal")):
+
+    # get the mean and standard deviation of the trend
     syst_mean = np.mean(syst_y)
     syst_std = np.std(syst_y)
 
+    # plot the trend, std. dev. band, and mean line
     style.errorbar(data_x, syst_y, syst_err_y)
-    style.horizontal_spread(syst_std, syst_mean, label = f"spread = {syst_std:.2f} {y_unit}")
+    style.horizontal_spread(syst_std, syst_mean, label = f"std. dev. = {syst_std:.2f} {y_unit}")
     style.draw_horizontal(syst_mean, label = f"mean = {syst_mean:.2f} {y_unit}")
 
-    style.label_and_save(x_label, f"{y_label} Discrepancy from Toy MC" if mode == "diff" else y_label, output)
+    # apply labels, legend, and save to output
+    style.label_and_save(x_label, f"{y_label} Difference from Toy MC" if mode == "diff" else y_label, output)
 
+  # assemble results object
   return Results({
     f"mean_{variable}": np.mean(data_y),
     f"std_{variable}": np.std(data_y),
+    f"range_{variable}": np.max(data_y) - np.min(data_y),
     f"diff_mean_{variable}": np.mean(data_y - sim_y),
-    f"diff_std_{variable}": np.std(data_y - sim_y)
+    f"diff_std_{variable}": np.std(data_y - sim_y),
+    f"diff_range_{variable}": np.max(data_y - sim_y) - np.min(data_y - sim_y)
   })
 
 # ==================================================================================================
