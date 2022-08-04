5aafff2b1c4c429372d16759188e79f5daec6c09
Tyler <tjb269@cornell.edu>
2022-08-04 15:48:19 -0400

    added collect_systematics script

---
+++ scripts/collect_systematics.py
@@ -0,0 +1,47 @@
+from gm2fr.src.Results import Results
+import gm2fr.src.io as io
+import argparse
+
+systematics = [
+  "start",
+  "end",
+  "df",
+  "freq_width"
+]
+
+diff_mode = [
+  "start",
+  "freq_width"
+]
+
+variables = [
+  "x",
+  "std_x",
+  "c_e",
+  "t0"
+]
+
+# ==================================================================================================
+
+def collect_systematics():
+
+  results = Results()
+  for i, systematic in enumerate(systematics):
+
+    syst_results = Results.load(f"{io.results_path}/systematics/systematic_{systematic}_results.npy")
+
+    if i == 0:
+      # copy the dataset column
+      results.table["dataset"] = syst_results.table["dataset"]
+
+    for variable in variables:
+      prefix = "diff_" if systematic in diff_mode else ""
+      results.table[f"{systematic}_std_{variable}"] = syst_results.table[f"{prefix}std_{variable}"]
+      results.table[f"{systematic}_range_{variable}"] = syst_results.table[f"{prefix}range_{variable}"]
+
+  results.save(f"{io.results_path}/systematics", "all_systematics")
+
+# ==================================================================================================
+
+if __name__ == "__main__":
+  collect_systematics(dataset)
