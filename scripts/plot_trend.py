764544a477e9e07cbd137bd7a92736678d4a6526
Tyler <tjb269@cornell.edu>
2022-09-26 14:57:13 -0400

    changed errorbar disabling criterion in plot_trend

--- scripts/plot_trend.py
+++ scripts/plot_trend.py
@@ -12,15 +12,16 @@ from gm2fr.src.Results import Results
 
 def plot_trend(x, y, results, label = None, ls = "-", color = None, skip = 1):
 
-  if f"err_{y}" in results.dtype.names:
-    errors = results[f"err_{y}"]
-    median_err = np.median(errors)
-    errors[errors > 10*median_err] = 0
-  else:
-    errors = None
-
   x_data = results[x] if x in results.dtype.names else x
   y_data = results[y]
+
+  if f"err_{y}" in results.dtype.names:
+    errors = results[f"err_{y}"]
+    # Don't plot error bars if they obscure the variation of the data.
+    y_range = np.abs(np.max(y_data) - np.min(y_data))
+    errors[errors > 2 * y_range] = 0
+  else:
+    errors = None
 
   if y == "t0": # patch the output for t0 being in microseconds
     y_data = y_data * 1E3
